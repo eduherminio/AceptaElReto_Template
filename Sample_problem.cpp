6c9dc022416c2c4ffc03d33ab792e1d79e048d1c
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-10-25 00:59:56 +0200

    Clean template

---
+++ Sample_problem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+    std::cout << str << std::endl;
+}
